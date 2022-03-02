2e42a71e1e193aea5a3f0c77fdd85c65c69c10c2
Monge1331 <gabriximenes100@gmail.com>
2022-03-01 22:00:31 -0300

    :up: Side bar finished

--- Utils/Script.js
+++ Utils/Script.js
@@ -1,1 +1,28 @@
-let List = document.querySelectorAll('.Navgation li');
+  // MAKE THE EFFECT HOVERD IN "LI" USING JAVASCRIPT
+
+  // TAKED LI IF DOOM
+  let list = document.querySelectorAll('.Navgation li');
+
+  // CREATING FUNCTION ACTIVE LINK WHERE WILL ADD AND REMOVE HOVERED
+ function ActiveLink(event){
+    list.forEach(x => x.classList.remove('hovered'))
+    event.target.classList.add('hovered')
+    
+}
+
+  // ADD EVENT TO MOUSEOVER!
+  list.forEach(e => e.addEventListener('mouseover', ActiveLink));
+
+
+  // MENU HAMBURGUER 
+
+  const navegation = document.querySelector('.Navgation');
+  const toggle = document.querySelector('.Toggle');
+  const main = document.querySelector('main');
+
+
+  toggle.onclick = () =>{
+    navegation.classList.toggle('active');
+    main.classList.toggle('active');
+
+  }
